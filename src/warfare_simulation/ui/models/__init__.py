e846d386ceefa96fe6d90c6baa8524956098999b
PinKing141 <favourezeh022@gmail.com>
2026-06-23 23:05:25 +0100

    Advance Living Chronicle observer dashboard

--- src/warfare_simulation/ui/models/__init__.py
+++ src/warfare_simulation/ui/models/__init__.py
@@ -1,13 +1,15 @@
 """Qt table models for the campaign dashboard."""
 
-from .province_model import ProvinceTableModel
-from .resource_model import ResourceTableModel
 from .event_model import EventTableModel
 from .faction_model import FactionTableModel
+from .observer_summary_model import ObserverSummaryTableModel
+from .province_model import ProvinceTableModel
+from .resource_model import ResourceTableModel
 
 __all__ = [
     "ProvinceTableModel",
     "ResourceTableModel",
     "EventTableModel",
     "FactionTableModel",
+    "ObserverSummaryTableModel",
 ]
